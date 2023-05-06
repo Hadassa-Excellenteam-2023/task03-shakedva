4fcc00eee7a1452607bc63f899d448dde5d6106a
shakedva <shakediii123@gmail.com>
2023-05-06 13:57:24 +0300

    Add documentation

--- include/PriorityQueue.h
+++ include/PriorityQueue.h
@@ -4,36 +4,38 @@
 #include <list>
 #include <exception>
 
-//struct MyComparator
-//{
-//	int operator()(int a, int b) const {
-//		return a - b;
-//	}
-//
-//};
-
 template <typename T>
 struct MyComparator
 {
+	/*
+	* Compare two values, returns their difference. 
+	* a > b		positive difference
+	* a == b	zero
+	* a < b		negative difference
+	*/
 	T operator()(T a, T b) const {
 		return a - b;
 	}
-
 };
 
 template <typename T>
 class PriorityQueue
 {
 public:
+	PriorityQueue() = default;
+	~PriorityQueue() = default;
+
+	// Push new element into the priority queue.
 	void push(const T& t)
 	{
 		auto iterator = _data.begin();
-		// Find the location to insert t as the list's value are from small to big. 
+		// Find the location to insert as the list's value are from small to big. 
 		while (iterator != _data.end() && cmp(*iterator, t) < 0)
 			iterator++;
 
 		_data.insert(iterator, t);
 	}
+	// Returns and takes out the smallest value from the priority queue.
 	T poll()
 	{
 		if (_data.empty())
